92d9133dccda64a30c7b0c2217e7fb508c17bfcc
Zhabskyi <poltavagaz@gmail.com>
2019-12-10 02:55:57 +0000

    Add callback on destroy method

--- lib/store.rb
+++ lib/store.rb
@@ -9,4 +9,13 @@ class Store < ActiveRecord::Base
       errors.add(:mens_apparel || :womens_apparel, "Store must carry at least one of mens apparel or womens apparel")
     end
   end
+
+  before_destroy :check_if_can_be_destroyed
+ 
+  private
+    def check_if_can_be_destroyed
+      if employees.any?
+        return false
+      end
+    end
 end
